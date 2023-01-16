edec41f908cda92bc8af4ee5715e3ec7e6547700
f0rtun-4 <sangadpintar@gmail.com>
2023-01-16 23:07:45 +0800

    refactor: style & layout

--- js/index.js
+++ js/index.js
@@ -12,3 +12,8 @@ $(".option-dropdown .dropdown-menu li").click( function () {
     $(this).parents(".option-dropdown").find("span").text($(this).text());
     $(this).parents(".option-dropdown").find("input").attr("value", $(this).attr("id"));
 });
+
+$(".navbar .container .dropdown").click(function () {
+    $(this).attr("tabindex", 1).focus();
+    $(this).find(".dropdown-content").slideToggle(300);  
+})
