df3204c3144cd66123499dcdf3f4fff526ff82c6
f0rtun-4 <sangadpintar@gmail.com>
2023-01-20 01:17:52 +0800

    refactor: style

--- js/index.js
+++ js/index.js
@@ -13,7 +13,27 @@ $(".option-dropdown .dropdown-menu li").click( function () {
     $(this).parents(".option-dropdown").find("input").attr("value", $(this).attr("id"));
 });
 
-$(".navbar .container .dropdown").click(function () {
+$(".dropdown-navbar").click(function () {
     $(this).attr("tabindex", 1).focus();
-    $(this).find(".dropdown-content").slideToggle(300);  
+    $(this).find(".dropdown-content-navbar").slideToggle(300);
+});
+
+$(".dropdown-navbar").focusout(function () {
+    $(this).find(".dropdown-content-navbar").slideUp(300);
+});
+
+// Card action Galery
+$(".like-icon").click(function () {
+    $(this).toggleClass("bi-heart")
+    $(this).toggleClass("bi-heart-fill")
+    console.log(1);
 })
+
+// window.onload = function () {
+//     const love = document.querySelector(".bi.bi-heart-fill");
+//     console.log(love);
+//     love.addEventListener("click", function (e) {
+//         love.style.color = 'red'
+//         console.log(1);
+//     });
+// }
